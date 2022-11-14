008cf22d1dbd74e77691e169b7516babae170461
Dennis Jackson <dennisdjack1987@gmail.com>
2022-11-14 00:40:24 -0800

    add model and seeding database

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,18 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+      
+    },
+    category_name:{
+      type:DataTypes.STRING,
+      allowNull: false
+
+    }
+
   },
   {
     sequelize,
